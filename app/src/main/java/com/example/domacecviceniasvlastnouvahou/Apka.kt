27289d9a5f00cc7990639abef9c1fa994cf0332f
Kubinqo <plavka3@stud.uniza.sk>
2023-04-16 13:40:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/domacecviceniasvlastnouvahou/Apka.kt
@@ -0,0 +1,11 @@
+package com.example.domacecviceniasvlastnouvahou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Apka : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/domacecviceniasvlastnouvahou/Pouzivatel.kt
@@ -0,0 +1,4 @@
+package com.example.domacecviceniasvlastnouvahou
+
+class Pouzivatel {
+}
