369a00e2f7739ae3ce62a9c9116e465623deea30
Kubinqo <plavka3@stud.uniza.sk>
2023-04-30 17:25:05 +0200

    implementacia aktivit, spravenie vyberu pohlavia - vsetko funkcne.

--- app/src/main/java/com/example/domacecviceniasvlastnouvahou/Apka.kt
+++ app/src/main/java/com/example/domacecviceniasvlastnouvahou/Apka.kt
@@ -2,6 +2,7 @@ package com.example.domacecviceniasvlastnouvahou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Apka : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
